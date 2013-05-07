10d3a325ef3b816b15c67f8e7586b9308767fa4c
J. Michael Dean <mike.dean@hsc.utah.edu>
2013-05-07 10:23:43 -0400

    Removed annoying System.print statements from the ICU service implementation.

--- edu.utah.cdmcc.glucose.tutorial.services/src/edu/utah/cdmcc/glucose/tutorial/services/IntensiveCareUnitServiceImpl.java
+++ edu.utah.cdmcc.glucose.tutorial.services/src/edu/utah/cdmcc/glucose/tutorial/services/IntensiveCareUnitServiceImpl.java
@@ -4,19 +4,12 @@ import glucose.GlucoseFactory;
 import glucose.IntensiveCareUnit;
 import glucose.IntensiveCareUnitService;
 
-
 public class IntensiveCareUnitServiceImpl implements IntensiveCareUnitService {
 
-	//IntensiveCareUnit icu = GlucoseFactory.eINSTANCE.createExampleGlucoseModel();
 	IntensiveCareUnit icu = GlucoseFactory.eINSTANCE.createIntensiveCareUnit();
 	
 	@Override
 	public IntensiveCareUnit getRootGroup() {
-		System.out.println("Inside implementation of ICU service");
-		System.out.println(icu);
 		return icu;
 	}
-	
-
-
 }
